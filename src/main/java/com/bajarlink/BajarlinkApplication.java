fdb8ce031516f97976cf3aa85d4fca24f975f069
tarikul <mdtarikulhassan5432@gmail.com>
2026-05-11 16:36:02 +0530

    create and run bajarlink pos project

---
+++ src/main/java/com/bajarlink/BajarlinkApplication.java
@@ -0,0 +1,13 @@
+package com.bajarlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajarlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BajarlinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bajarlink/BajarlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajarlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajarlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
